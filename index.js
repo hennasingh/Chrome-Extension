01278adb9dc67bdbd7744b76f353e90bc54f3f41
Henna Singh <hennasingh04@gmail.com>
2023-09-06 22:55:08 +0100

    added click listener to add values to an array and used innerHTML to display elements in unordered list

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let inputBtn = document.getElementById("input-btn");
+
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function() {
-    
+    myLeads.push(inputEl.value)
+    console.log(myLeads);
 })
+
+for(let i=0; i < myLeads.length; i ++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "<li>"
+}
+
